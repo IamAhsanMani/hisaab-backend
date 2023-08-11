fbc2ad4ea9fe2a8cfdd2c6f4eb49142b827a3439
iamahsanmani <ahsanmani01@gmail.com>
2023-08-11 09:00:40 +0430

    added env

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 const firebase = require('firebase-admin');
 
 var serviceAccount = require("./serviceAccountKey.json");
+const dotenv = require('dotenv').config({path: './.env'});
+
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
-    databaseURL: 'https://hisaab-a86f5-default-rtdb.firebaseio.com',
+    databaseURL:  process.env.DB_URL,
 });
 
 exports.admin = firebase;
