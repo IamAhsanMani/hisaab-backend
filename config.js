1917dfe5ac9cdad61fb40b861aa3ef3ebe951ac8
iamahsanmani <ahsanmani01@gmail.com>
2023-08-11 10:32:46 +0430

    added firebase cred

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 const firebase = require('firebase-admin');
 
-var serviceAccount = require("./serviceAccountKey.json");
-const dotenv = require('dotenv').config({path: './.env'});
+// var serviceAccount = require("./serviceAccountKey.json");
+require('dotenv').config();
+const serviceAccount = process.env.GOOGLE_CREDS;
+
+// if (!serviceAccount) throw new Error('The $GOOGLE_AUTH_JSON environment variable was not found!');
 
 // const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 firebase.initializeApp({
-    credential: firebase.credential.cert(serviceAccount),
+    credential: firebase.credential.cert(JSON.parse(serviceAccount)),
     databaseURL:  process.env.DB_URL,
 });
 
